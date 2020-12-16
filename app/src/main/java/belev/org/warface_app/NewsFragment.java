9e01329721f97090851600ce06078f9a62d371c0
Roman <ram219219@gmail.com>
2020-12-16 12:47:44 +0200

    Release test 2

--- app/src/main/java/belev/org/warface_app/NewsFragment.java
+++ app/src/main/java/belev/org/warface_app/NewsFragment.java
@@ -1,26 +1,12 @@
 package belev.org.warface_app;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-import com.google.android.gms.ads.InterstitialAd;
-import com.google.gson.Gson;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import androidx.annotation.Nullable;
@@ -31,8 +17,6 @@ public class NewsFragment extends ListFragment {
     android.widget.Adapter adapter;
     public static final String BUNDLE_TEXT = "bundle_text";
     public List<NewsModel> newsModelList = new ArrayList<>();
-    InterstitialAd mInterstitialAd;
-    // List<NewsModel> newsList;
     MainActivity mainActivity;
     boolean isLoadedAd;
 
@@ -42,7 +26,6 @@ public class NewsFragment extends ListFragment {
         ListView listView = (ListView) view.findViewById(android.R.id.list);
         View spaceView = new View(getContext());
         mainActivity = (MainActivity) getActivity();
-        // newsList = mainActivity.newsList;
 
         NativeAdPopulationSync nativeAdPopulationSync = new NativeAdPopulationSync(0);
         nativeAdPopulationSync.view = view;
@@ -91,76 +74,6 @@ public class NewsFragment extends ListFragment {
             task.makeTask(mainActivity.newsList);
         }
     }
-
-    /*
-    public class JSONTask extends AsyncTask<String, String, List<NewsModel> > {
-
-        @Override
-        protected List doInBackground(String... params) {
-
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
-
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line);
-                }
-
-                String finalJson = buffer.toString();
-
-                JSONObject parentObject = new JSONObject(finalJson);
-                JSONArray parentArray = parentObject.getJSONArray("movies");
-
-                //List<NewsModel> newsModelList = new ArrayList<>();
-
-                Gson gson = new Gson();
-                for (int i = 0; i < parentArray.length(); i++) {
-                    JSONObject finalObject = parentArray.getJSONObject(i);
-                    NewsModel newsModel = gson.fromJson(finalObject.toString(), NewsModel.class);
-                    newsModelList.add(newsModel);
-                }
-                return newsModelList;
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null)
-                        reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(final List<NewsModel> result) {
-            super.onPostExecute(result);
-            if(result != null) {
-                adapter = new NewsAdapter(getActivity(), result);
-                setListAdapter((ListAdapter) adapter);
-            } else {
-                Toast.makeText(getActivity(), "Error Json Reader", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-    */
 
     @Override
     public void onDestroy() {
--- app/src/main/java/belev/org/warface_app/StartFragment.java
+++ app/src/main/java/belev/org/warface_app/StartFragment.java
@@ -6,12 +6,32 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class StartFragment extends Fragment {
 
-    Uri link;
+    public BillingClient billingClient;
+    public MainActivity mainActivity;
+    public SkuDetails skuDetails;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -21,7 +41,96 @@ public class StartFragment extends Fragment {
         String text = getResources().getString(R.string.start_text);
         textView.setText(Html.fromHtml(text));
 
+        mainActivity = (MainActivity) getActivity();
+
+        Button buttonDonate = (Button) view.findViewById(R.id.button_donate);
+        buttonDonate.setOnClickListener(buttonClickHandler());
+
+        initBillingClient();
+
         return view;
+    }
+
+    public void initBillingClient() {
+
+        PurchasesUpdatedListener purchasesUpdatedListener = new PurchasesUpdatedListener() {
+            @Override
+            public void onPurchasesUpdated(@NonNull BillingResult billingResult, @Nullable List<Purchase> list) {
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK
+                        && list != null) {
+                    for (Purchase purchase : list) {
+                        // handlePurchase(purchase);
+                    }
+                }
+            }
+        };
+
+        billingClient = BillingClient.newBuilder(mainActivity)
+                .setListener(purchasesUpdatedListener)
+                .enablePendingPurchases()
+                .build();
+
+        billingClient.startConnection(new BillingClientStateListener() {
+            @Override
+            public void onBillingSetupFinished(BillingResult billingResult) {
+                if (billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK) {
+                    System.out.println("Ok connect billing client -------------->");
+
+                    List<String> skuList = new ArrayList<String>();
+                    skuList.add("premium_level_1");
+
+                    SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
+                    params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP);
+
+                    billingClient.querySkuDetailsAsync(params.build(), new SkuDetailsResponseListener() {
+                        @Override
+                        public void onSkuDetailsResponse(@NonNull BillingResult billingResult, @Nullable List<SkuDetails> list) {
+                            System.out.println(list.get(0).getPrice());
+                            skuDetails = list.get(0);
+                        }
+                    });
+                } else {
+                    System.out.println("Error connect billing client -------------->");
+                }
+            }
+
+            @Override
+            public void onBillingServiceDisconnected() {
+                System.out.println("Disconnected billing client -------------->");
+            }
+        });
+
+        /*
+        List<String> skuList = new ArrayList<String>();
+        skuList.add("premium_level_1");
+
+        SkuDetailsParams.Builder params = SkuDetailsParams.newBuilder();
+        params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP);
+
+        billingClient.querySkuDetailsAsync(params.build(), new SkuDetailsResponseListener() {
+            @Override
+            public void onSkuDetailsResponse(@NonNull BillingResult billingResult, @Nullable List<SkuDetails> list) {
+                System.out.println(list.get(0).getPrice());
+            }
+        });
+        */
+    }
+
+    public View.OnClickListener buttonClickHandler() {
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Button click ------------------------------------>");
+
+                BillingFlowParams billingFlowParams = BillingFlowParams.newBuilder()
+                        .setSkuDetails(skuDetails)
+                        .build();
+
+                int responseCode = billingClient.launchBillingFlow(mainActivity, billingFlowParams).getResponseCode();
+            }
+        };
+
+        return onClickListener;
     }
 
 }
