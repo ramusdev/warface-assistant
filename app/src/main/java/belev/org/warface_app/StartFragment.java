cb83d359f93f0e7799ae75ec1d02ea39bb86502e
Roman <ram219219@gmail.com>
2020-12-27 21:00:58 +0200

    Style to notification

--- app/src/main/java/belev/org/warface_app/StartFragment.java
+++ app/src/main/java/belev/org/warface_app/StartFragment.java
@@ -3,13 +3,21 @@ package belev.org.warface_app;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.AbsoluteSizeSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -200,8 +208,13 @@ public class StartFragment extends Fragment implements View.OnClickListener {
         String channelId = "channel_id_101";
         String channelDescription = "Chanell main";
         String name = "channel_name";
+        String text = "Чем заняться в ближайшее время? В игре вас ожидает множество активностей! Читайте подробности, чтобы узнать о всех нововведениях.";
         int notifyId = 100;
         int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+        Intent intent = new Intent(mainActivity, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(mainActivity, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
         NotificationChannel notificationChannel = new NotificationChannel(channelId, name, importance);
         notificationChannel.setDescription(channelDescription);
@@ -211,18 +224,26 @@ public class StartFragment extends Fragment implements View.OnClickListener {
         NotificationManager notificationManager = (NotificationManager) mainActivity.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.createNotificationChannel(notificationChannel);
 
+        SpannableString spannableString = new SpannableString("Календарь новогодних событий в Warface");
+        spannableString.setSpan(new StyleSpan(Typeface.BOLD), 0, spannableString.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
         Notification notification = new Notification.Builder(mainActivity)
                 .setSmallIcon(R.drawable.ic_stat_warface_icon)
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-                .setContentTitle("Title")
-                .setContentText("Content text")
                 .setChannelId(channelId)
                 .setPriority(Notification.PRIORITY_MAX)
                 .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                // .setStyle(new Notification
+                        // .BigTextStyle()
+                        // .bigText(text))
+                .setContentTitle(spannableString)
+                .setContentText(text)
                 .build();
 
         notificationManager.notify(notifyId, notification);
 
+        // .BigTextStyle().bigText(text))
         // NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(mainActivity);
         // notificationManagerCompat.notify(notifyId, builder.build());
 
