2ab09f12f9f75348af09769168511c599423fd3e
Roman <ram219219@gmail.com>
2021-02-06 16:00:25 +0200

    Translation v1.1

--- app/src/main/java/belev/org/warface_app/NewsParser.java
+++ app/src/main/java/belev/org/warface_app/NewsParser.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class NewsParser {
 
-    private final String URL_CONNECT = "https://ru.warface.com/rss.xml";
+    // private final String URL_CONNECT = "https://ru.warface.com/rss.xml";
     private final String USER_AGENT = "Chrome/4.0.249.0 Safari/532.5";
     private final String REFERRER = "https://www.google.com";
     private final int COUNT_NEWS = 10;
